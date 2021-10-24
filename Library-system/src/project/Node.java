16faee0520ff68aa627e851832675f82ade62f0f
Mohamed <67746067+7mdiano@users.noreply.github.com>
2021-10-24 08:20:15 +0200

    Add files via upload

---
+++ Library-system/src/project/Node.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Mohamed
+ */
+public class Node {
+    int ID, num;
+    double price;
+    String Title, Author, category;
+    
+    Node Next;
+    Node Prev;
+    
+}
+
+
